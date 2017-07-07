bbd27b62e7a97223cf898903e404354f54f64236
李志权 <lizhiquan@lizhiquandeiMac.local>
2017-07-07 11:29:55 +0800

    aa

---
+++ 顶部提示/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  顶部提示
+//
+//  Created by YYKit on 2017/5/27.
+//  Copyright © 2017年 YZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 顶部提示/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  顶部提示
+//
+//  Created by YYKit on 2017/5/27.
+//  Copyright © 2017年 YZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
